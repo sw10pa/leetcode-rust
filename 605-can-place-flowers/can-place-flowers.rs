bafe7b42e718dea7e9ce1578f5456e155f1961d2
Stephane Gurgenidze <59443568+sw10pa@users.noreply.github.com>
2024-11-07 03:40:28 +0100

    Time: 1 ms (13.95%) | Memory: 2.3 MB (54.13%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    fn can_be_planted_in(index: usize, flowerbed: &Vec<i32>) -> bool {
+        (flowerbed[index - 1] == 0 && flowerbed[index] == 0 && flowerbed[index + 1] == 0)
+    }
+
+    pub fn can_place_flowers(flowerbed: Vec<i32>, n: i32) -> bool {
+        let (mut flowerbed, mut n) = (flowerbed, n);
+
+        flowerbed.insert(0, 0);
+        flowerbed.push(0);
+
+        for index in 1..flowerbed.len() - 1 {
+            if n == 0 {
+                break;
+            }
+
+            if Self::can_be_planted_in(index, &flowerbed) {
+                flowerbed[index] = 1;
+                n -= 1;
+            }
+        }
+
+        (n == 0)
+    }
+}
