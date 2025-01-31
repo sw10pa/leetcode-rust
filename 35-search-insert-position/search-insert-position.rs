5cfb68b1b678fe2b0f80684eae6965bea4bdc80b
Stephane Gurgenidze <59443568+sw10pa@users.noreply.github.com>
2025-01-31 17:45:12 +0400

    Time: 0 ms (100.00%) | Memory: 2.4 MB (43.15%) - LeetSync

--- 35-search-insert-position/search-insert-position.rs
+++ 35-search-insert-position/search-insert-position.rs
@@ -4,17 +4,16 @@ impl Solution {
 
         while left <= right {
             let mut middle = (left + right) / 2;
-            println!("{}", middle);
 
             if target < nums[middle as usize] {
                 right = middle - 1;
             } else if target > nums[middle as usize] {
                 left = middle + 1;
             } else {
-                return middle as i32;
+                return middle;
             }
         }
 
-        left as i32
+        left
     }
 }
