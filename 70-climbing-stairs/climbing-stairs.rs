79f29f02418e666bb7c1ec653c1ddedb3c32c695
Stephane Gurgenidze <59443568+sw10pa@users.noreply.github.com>
2025-02-06 03:18:09 +0400

    Time: 0 ms (100.00%) | Memory: 2.3 MB (3.71%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.rs
+++ 70-climbing-stairs/climbing-stairs.rs
@@ -1,10 +1,10 @@
 impl Solution {
     pub fn climb_stairs(n: i32) -> i32 {
-        let (mut fib1, mut fib2, mut fib3) = (1, 1, 2);
-        for _ in 0..n {
+        let (mut fib1, mut fib2) = (1, 2);
+        for _ in 1..n {
+            let temp = fib1 + fib2;
             fib1 = fib2;
-            fib2 = fib3;
-            fib3 = fib1 + fib2;
+            fib2 = temp;
         }
         fib1
     }
