1a1c56ad90c970cd6bff12ba04bca44148c448bc
Stephane Gurgenidze <59443568+sw10pa@users.noreply.github.com>
2025-02-11 03:38:35 +0400

    Time: 0 ms (100.00%) | Memory: 2.4 MB (95.52%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.rs
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.rs
@@ -2,18 +2,22 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn first_uniq_char(s: String) -> i32 {
-        let mut letters: HashMap<char, u32> = HashMap::new();
+        let mut letters = vec![0; 26];
 
-        for ch in s.chars() {
-            *letters.entry(ch).or_insert(0) += 1;
+        for ch in s.bytes() {
+            letters[Self::get_char_index(ch)] += 1;
         }
 
-        for (index, ch) in s.chars().enumerate() {
-            if letters[&ch] == 1 {
+        for (index, ch) in s.bytes().enumerate() {
+            if letters[Self::get_char_index(ch)] == 1 {
                 return index as i32;
             }
         }
 
         -1
+    }
+
+    fn get_char_index(ch: u8) -> usize {
+        (ch - b'a') as usize
     }
 }
