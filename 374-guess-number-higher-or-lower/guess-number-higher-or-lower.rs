5a9c72cf55f01f47a16c4242273c7123e6320bed
Stephane Gurgenidze <59443568+sw10pa@users.noreply.github.com>
2025-02-11 03:22:51 +0400

    Time: 0 ms (100.00%) | Memory: 2.3 MB (1.08%) - LeetSync

---
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.rs
@@ -0,0 +1,24 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * unsafe fn guess(num: i32) -> i32 {}
+ */
+
+impl Solution {
+    unsafe fn guessNumber(n: i32) -> i32 {
+        let (mut left, mut right) = (1, n);
+        loop {
+            let middle = left + (right - left) / 2;
+            match guess(middle) {
+                -1 => right = middle - 1,
+                1 => left = middle + 1,
+                _ => {
+                    return middle;
+                },
+            }
+        }
+    }
+}
